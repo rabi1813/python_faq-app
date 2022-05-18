ac5ab4524ee89041fa12a0bed5b7e660f8de478b
Rabindra Kumar Sahu <rabindra1813@gmail.com>
2022-05-18 22:17:23 +0530

    Angular Configuration

---
+++ src/app/_helpers/jwt.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { finalize } from "rxjs/operators";
+import { v4 as uuid } from 'uuid';
+import { environment } from "src/environments/environment";
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    public unique_request_id: string = "";
+
+    constructor() {}
+    
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        this.unique_request_id = uuid()
+        request = request.clone({
+            setHeaders: {
+                "Transaction-Id": this.unique_request_id
+            }
+        });
+        return next.handle(request);
+    }
+    
+}
